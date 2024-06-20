3819f2eac1656b9744a0d88d48f73876bf971a5a
augustofaggionliradossantos <a.faggion.santos@gmail.com>
2024-06-20 22:44:23 +0200

    full responsible

--- javascript/toogle_navbar.js
+++ javascript/toogle_navbar.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarLinks = Array.prototype.slice.call(document.querySelectorAll('.navbar-menu a'), 0);
 
   // Add a click event on each of them
   $navbarBurgers.forEach(el => {
@@ -12,6 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       el.classList.toggle('is-active');
       $target.classList.toggle('is-active');
+    });
+  });
+
+  // Add a click event on each navbar link
+  $navbarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      // Get the "navbar-burger" and the "navbar-menu"
+      const $navbarBurger = document.querySelector('.navbar-burger');
+      const $navbarMenu = document.querySelector('.navbar-menu');
+
+      // Remove the "is-active" class from both the "navbar-burger" and the "navbar-menu"
+      $navbarBurger.classList.remove('is-active');
+      $navbarMenu.classList.remove('is-active');
     });
   });
 });
